b3ad7ab5c9841f89d7ce779090776cf8fc60961a
Alex Yates <alex.yates@dlmconsultants.com>
2020-10-05 17:45:13 +0100

    Unfinished: Converting build_dbServers.ps1 to a build everything script

---
+++ Infrastructure/helper_scripts/VM_UserData_DbServer.ps1
@@ -0,0 +1,77 @@
+<powershell>
+$ErrorActionPreference = "stop"
+
+$startupDir = "C:\Startup"
+$scriptsDir = "scripts"
+
+if ((test-path $startupDir) -ne $true) {
+  New-Item -ItemType "Directory" -Path $startupDir
+}
+
+Set-Location $startupDir
+
+# If for whatever reason this doesn't work, check this file:
+$log = ".\StartupLog.txt"
+Write-Output " Creating log file at $log"
+Start-Transcript -path $log -append
+
+Set-Location $startupDir
+
+if ((test-path $scriptsDir) -ne $true) {
+  New-Item -ItemType "Directory" -Path $scriptsDir
+}
+
+Set-Location $scriptsDir
+
+Function Get-Script{
+  param (
+    [Parameter(Mandatory=$true)][string]$script,
+    [string]$owner = "__REPOOWNER__",
+    [string]$repo = "__REPONAME__",
+    [string]$branch = "main",
+    [string]$path = "Infrastructure\UserDataDownloads"
+  )
+  $uri = "https://raw.githubusercontent.com/$owner/$repo/$branch/$path/$script"
+  Write-Output "Downloading $script"
+  Write-Output "  from: $uri"
+  Write-Output "  to: .\$script"
+  Invoke-WebRequest -Uri $uri -OutFile ".\$script" -Verbose
+}
+
+# Setting up users first, so that if anything goes wrong later, folks can RDP in to troubleshoot
+Write-Output "*"
+Get-Script -script "setup_users.ps1"
+Write-Output "Executing ./setup_users.ps1"
+./setup_users.ps1
+
+# Chocolatey is required for both SQL Server and SSMS installs
+Write-Output "*"
+Get-Script -script "install_choco.ps1"
+Write-Output "Executing ./install_choco.ps1"
+./install_choco.ps1
+
+# Installing SQL Server, using a specific config file
+Write-Output "*"
+Write-Output "Downloading ConfigurationFile.ini and install_sql_with_choco.ps1"
+Get-Script -script "ConfigurationFile.ini"
+Get-Script -script "install_sql_server.ps1"
+Write-Output "Executing ./install_sql_server.ps1"
+./install_sql_server.ps1
+
+# Creating SQL logins so that student and octopus can both access SQL Server
+Write-Output "*"
+Get-Script -script "setup_sql_logins.ps1"
+Write-Output "Executing ./setup_sql_logins.ps1"
+./setup_sql_logins.ps1
+
+# Installing SSMS for convenience. Not required to deploy anything so doing this last to avoid delays.
+Write-Output "*"
+Get-Script -script "install_ssms.ps1"
+Write-Output "Executing ./install_ssms.ps1"
+./install_ssms.ps1
+
+Write-Output "VM_UserData startup script completed..."
+</powershell>
+
+
+
--- Infrastructure/helper_scripts/build_dbServers.ps1
+++ Infrastructure/helper_scripts/build_dbServers.ps1
@@ -1,205 +1,298 @@
 param(
     $instanceType = "t2.micro", # 1 vCPU, 1GiB Mem, free tier elligible: https://aws.amazon.com/ec2/instance-types/
     $ami = "ami-0d2455a34bf134234", # Microsoft Windows Server 2019 Base with Containers
-    $dbServerRole = "RandomQuotes_SQL-DbServer",
-    $tagValue = "Created manually",
-    $octoUrl = "",
-    $octoEnv = "",
-    [Switch]$Wait,
+    $numWebServers = 1,
     $timeout = 4800 # seconds
 )
 
 $ErrorActionPreference = "Stop"
+$stopwatch =  [system.diagnostics.stopwatch]::StartNew()
 
-# Reading VM_UserData
-$userDataFile = "VM_UserData_SQL.ps1"
-$userDataPath = "$PSScriptRoot\$userDataFile"
-$userData = Get-Content -Path $userDataPath -Raw
+# Initialising variables
+$rolePrefix= ""
+try {
+    $rolePrefix = $OctopusParameters["Octopus.Project.Name"]
+}
+catch {
+    $rolePrefix = "UnknownProject"
+}
 
-# Preparing startup script for VM
-if ($DeployTentacle){
-    # And substitute the octopus URL and environment
+$tagValue= ""
+try {
+    $tagValue = $OctopusParameters["Octopus.Environment.Name"]
+}
+catch {
+    $tagValue = "EnvironmentUnknown"
+}
+
+$octoUrl= ""
+try {
+    $octoUrl = $OctopusParameters["Octopus.Web.BaseUrl"]
+}
+catch {
+    $octoUrl = "OctopusUrlUnknown"
+}
+
+$webServerRole = "$rolePrefix-WebServer"
+$dbServerRole = "$rolePrefix-DbServer"
+$dbJumpboxRole = "$rolePrefix-DbJumpbox"
+
+$acceptableStates = @("pending", "running")
+
+$dbIpAddress = ""
+
+# Helper function to read and encoding the VM startup scripts
+Function Get-UserData {
+    param (
+        $fileName,
+        $role
+    )
+    
+    # retrieving raw source code
+    $userDataPath = "$PSScriptRoot\$filename"
+    if (-not (Test-Path $userDataPath)){
+        Write-Error "No UserData (VM startup script) found at $userDataPath!"
+    }
+    $userData = Get-Content -Path $userDataPath -Raw
+    
+    # replacing placeholder text
     $userData = $userData.replace("__OCTOPUSURL__",$octoUrl)
     $userData = $userData.replace("__ENV__",$octoEnv)
-    $userData = $userData.replace("__OCTOPUSURL__",$octoUrl)
-    $userData = $userData.replace("__ROLE__",$dbServerRole)
+    $userData = $userData.replace("__ROLE__",$role)
+
+    # Base 64 encoding the userdata file (required by EC2)
+    $encodedDbUserData = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($userData))
+
+    # returning encoded userdata
+    return $encodedDbUserData
 }
 
-if (Test-Path $userDataPath){
-    Write-Output "    Reading UserData (VM startup script) from $userDataPath."
-}
-else {
-    Write-Error "No UserData (VM startup script) found at $userDataPath!"
-}
-# Base 64 encoding the setup script. More info here: 
-# https://docs.aws.amazon.com/AWSEC2/latest/WindowsGuide/ec2-windows-user-data.html
-Write-Output "    Base 64 encoding UserData."
-$encodedUserData = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($userData))
+# Reading and encoding the VM startup scripts
+$webServerUserData = Get-UserData -fileName "VM_UserData_WebServer.ps1" -role $webServerRole
+$dbServerUserData = Get-UserData -fileName "VM_UserData_DbServer.ps1" -role $dbServerRole
+$jumpServerUserData = Get-UserData -fileName "VM_UserData_DbJumpbox.ps1" -role $dbJumpboxRole
 
-# Checking how many instances are already running
-Write-Output "    Checking how many instances are already running with tag $dbServerRole and value $tagValue..."
-$acceptableStates = @("pending", "running")
-$PreExistingInstances = (Get-EC2Instance -Filter @{Name="tag:$dbServerRole";Values=$tagValue}, @{Name="instance-state-name";Values=$acceptableStates}).Instances 
-$before = $PreExistingInstances.count
-Write-Output "      $before instances are already running." 
-$totalRequired = 1 - $PreExistingInstances.count # We only ever want 1 SQL instance per env!
-if ($totalRequired -lt 0){
-    $totalRequired = 0
-}
-Write-Output "      $totalRequired more instances required." 
-
-if ($totalRequired -gt 0){
-    Write-Output "    Launching $totalRequired instances of type $instanceType and ami $ami."
-    
-
-    Write-Output "      Instances will each have tag $dbServerRole with value $tagValue."
-
-    $NewInstance = New-EC2Instance -ImageId $ami -MinCount $totalRequired -MaxCount $totalRequired -InstanceType $instanceType -UserData $encodedUserData -KeyName RandomQuotes_SQL -SecurityGroup RandomQuotes_SQL -IamInstanceProfile_Name RandomQuotes_SQL
-
-    # Tagging all the instances
-    ForEach ($InstanceID  in ($NewInstance.Instances).InstanceId){
-        New-EC2Tag -Resources $( $InstanceID ) -Tags @(
-            @{ Key=$dbServerRole; Value=$tagValue}
-        );
+# Helper function to check if server already exists
+Function Test-Server {
+    param (
+        $role,
+        $value = $tagValue
+    )
+    $instances = (Get-EC2Instance -Filter @{Name="tag:$role";Values=$value}, @{Name="instance-state-name";Values=$acceptableStates}).Instances 
+    if ($instances.count -eq 0){
+        return $true
     }
+    return $false
 }
-# Initializing potential error data
-$oops = $false
-$err = "There is a problem with the following instances: "
 
-# Checking if it worked
-Write-Output "    Verifying that 1 SQL instance is running in $octoEnv environment: "
-$instances = (Get-EC2Instance -Filter @{Name="tag:$dbServerRole";Values=$tagValue}, @{Name="instance-state-name";Values=$acceptableStates}).Instances
+# Helper function to build a server if it doesn't already exist
+Function Build-Server {
+    param (
+        $role,
+        $value = $tagValue,
+        $encodedUserData,
+        $count = 1
+    )
+    if (Test-Server $dbServerRole){
+        $NewInstance = New-EC2Instance -ImageId $ami -MinCount $count -MaxCount 1 -InstanceType $instanceType -UserData $encodedUserData -KeyName RandomQuotes_SQL -SecurityGroup RandomQuotes_SQL -IamInstanceProfile_Name RandomQuotes_SQL
+        # Tagging all the instances
+        ForEach ($InstanceID  in ($NewInstance.Instances).InstanceId){
+            New-EC2Tag -Resources $( $InstanceID ) -Tags @(
+                @{ Key=$role; Value=$value}
+            );
+        }
+    }    
+}
 
-ForEach ($instance in $instances){
+# Building all the servers
+Build-Server -role $dbServerRole -encodedUserData $dbServerUserData
+Build-Server -role $dbJumpboxRole -encodedUserData $jumpServerUserData
+Build-Server -role $webServerRole -encodedUserData $webServerUserData -count $numWebServers
+
+# Checking all the instances
+$dbServerInstances = (Get-EC2Instance -Filter @{Name="tag:$dbServerRole";Values=$tagValue}, @{Name="instance-state-name";Values=$acceptableStates}).Instances
+$dbJumpboxInstances = (Get-EC2Instance -Filter @{Name="tag:$dbJumpboxRole";Values=$tagValue}, @{Name="instance-state-name";Values=$acceptableStates}).Instances
+$webServerInstances = (Get-EC2Instance -Filter @{Name="tag:$webServerRole";Values=$tagValue}, @{Name="instance-state-name";Values=$acceptableStates}).Instances
+
+# Logging all the instance details
+Write-Output "    Verifying SQL Server instance: "
+ForEach ($instance in $dbServerInstances){
     $id = $instance.InstanceId
     $state = $instance.State.Name
     Write-Output "      Instance $id is in state: $state"
 }
 
-if ($instances.count -ne $count){
-    $errmsg = "Expected to see $count instances, but actually see " + $instances.count + " instances."
-    Write-Warning "$errmsg"
-    $err = $err + ". Also, $errmsg"
-    $oops = $true
+Write-Output "    Verifying SQL Jumpbox instance: "
+ForEach ($instance in $dbJumpboxInstances){
+    $id = $instance.InstanceId
+    $state = $instance.State.Name
+    Write-Output "      Instance $id is in state: $state"
 }
 
-# Logging results
-if ($oops){
-    Write-Error $err
-} else {
-    $msg = "    " + $instances.count + " instances have been launched successfully."
-    Write-Output $msg
+Write-Output "    Verifying Web Server instance(s): "
+ForEach ($instance in $webServerInstances){
+    $id = $instance.InstanceId
+    $state = $instance.State.Name
+    Write-Output "      Instance $id is in state: $state"
 }
 
-if ($Wait -and ($totalRequired -gt 0)){
-    $allRunning = $false
-    $runningWarningGiven = $false
-    $ipAddress = ""
+# Checking we've got all the right instances
+$instancesFailed = $false
+$errMsg = ""
+if ($dbServerInstances.count -ne 1){
+    $instancesFailed = $true
+    $num = $dbServerInstances.count
+    $errMsg = "$errMsg Expected 1 SQL Server instance but have $num instance(s)."
+}
+if ($dbJumpboxInstances.count -ne 1){
+    $instancesFailed = $true
+    $num = $dbJumpboxInstances.count
+    $errMsg = "$errMsg Expected 1 SQL Jumpbox instance but have $num instance(s)."
+}
+if ($webServerInstances.count -ne $numWebServers){
+    $instancesFailed = $true
+    $num = $webServerInstances.count
+    $errMsg = "$errMsg Expected $numWebServers Web Server instance(s) but have $num instance(s)."
+}
+if ($instancesFailed){
+    Write-Error $errMsg
+}
+else {
+    Write-Output "    All instances launched successfully!"
+}
 
-    Write-Output "    Waiting for instances to start. (This normally takes about 30 seconds.)"
-    $stopwatch =  [system.diagnostics.stopwatch]::StartNew()
+Write-Output "      $time seconds | Waiting for instances to start... (This normally takes about 30 seconds.)"
 
-    While (-not $allRunning){
-        $time = [Math]::Floor([decimal]($stopwatch.Elapsed.TotalSeconds))
-        
-        if ($time -gt $timeout){
-            Write-Error "Timed out at $time seconds. Timeout currently set to $timeout seconds. There is a parameter on this script to adjust the default timeout."
-        }
-        
-        if (($time -gt 60) -and (-not $runningWarningGiven)){
-            Write-Warning "EC2 instances are taking an unusually long time to start."
-            $runningWarningGiven = $true
-        }
-        
-        $runningInstances = (Get-EC2Instance -Filter @{Name="tag:$dbServerRole";Values=$tagValue}, @{Name="instance-state-name";Values="running"}).Instances
-        $NumRunning = $runningInstances.count
-        
-        if ($NumRunning -eq $count){
-            $allRunning = $true
-            Write-Output "      $time seconds: All instances are running!"
-            ForEach ($instance in $runningInstances){
-                $id = $instance.InstanceId
-                $ipAddress = $instance.PublicIpAddress
-                Write-Output "        Instance $id is available at the public IP: $ipAddress"
-            }
-            break
-        }
-        else {
-            Write-Output "      $time seconds: $NumRunning out of $count instances are running."
-        }
+$allRunning = $false
+While (-not $allRunning){
+    
+    $dbServerInstances = (Get-EC2Instance -Filter @{Name="tag:$dbServerRole";Values=$tagValue}, @{Name="instance-state-name";Values="running"}).Instances
+    $dbJumpboxInstances = (Get-EC2Instance -Filter @{Name="tag:$dbJumpboxRole";Values=$tagValue}, @{Name="instance-state-name";Values="running"}).Instances
+    $webServerInstances = (Get-EC2Instance -Filter @{Name="tag:$webServerRole";Values=$tagValue}, @{Name="instance-state-name";Values="running"}).Instances
+    
+    $allInstances = @()
+    $allInstances += $dbServerInstances 
+    $allInstances += $dbJumpboxInstances 
+    $allInstances += $webServerInstances 
 
-        Start-Sleep -s 10
+    $NumRunning = $allInstances.count
+    
+    $time = [Math]::Floor([decimal]($stopwatch.Elapsed.TotalSeconds))
+
+    if ($NumRunning -eq ($numWebServers + 2)){
+        $allRunning = $true
+        Write-Output "      $time seconds | All instances are running!"
+        $dbIpAddress = $dbServerInstances[0].PublicIpAddress
+        ForEach ($instance in $runningInstances){
+            $id = $instance.InstanceId
+            $ipAddress = $instance.PublicIpAddress
+            Write-Output "        Instance $id is available at the public IP: $ipAddress"
+        }
+        break
     }
+    else {
+        Write-Output "      $time seconds | $NumRunning out of $numWebServers instances are running."
+    }
+    Start-Sleep -s 15
+}
 
-    try {
-        Import-Module dbatools
+# Installing dbatools PowerShell module so that we can ping sql server instance
+try {
+    Import-Module dbatools
+}
+catch {
+    Write-Output "    Installing dbatools so that we can ping SQL Server..."
+    Write-Output "      (This takes a couple of minutes)"
+    Install-Module dbatools -Force
+}
+
+Write-Output "      $time seconds | Waiting for instances to become responsive... (This normally takes about 5 minutes.)"
+
+# Helper functions to ping the instances
+function Test-SQL {
+    param (
+        $cred
+    )
+    try { 
+        Invoke-DbaQuery -SqlInstance $dbIpAddress -Query 'SELECT @@version' -SqlCredential $cred -EnableException
     }
     catch {
-        Write-Output "    Installing dbatools so that we can ping SQL Server..."
-        Write-Output "      (This takes a couple of minutes)"
-        Install-Module dbatools -Force
+        return $false
+    }
+    return $true
+}
+
+function Test-IIS {
+    Write-Warning "TO DO: Write a Test-IIS function!"
+}
+
+function Get-Tentacles {
+    param (
+        $role
+    )
+    Write-output "Role: $role"
+    Write-Warning "TO DO: Write a Get-Tentacles function!"
+}
+
+Write-Warning "Consider creating a hashtable or something for all websevers to keep track of IIS/tentacles etc"
+
+# Waiting to see if they all come online
+$allListening = $false
+$runningWarningGiven = $false
+$SqlOnline = $false
+$LoginsDeployed = $false
+$JumpboxListening = $false
+$IssInstalls = 0
+$WebServersListening = 0
+
+$saPassword = $OctopusParameters["sqlSaPassword"] | ConvertTo-SecureString -AsPlainText -Force
+$saUsername = "sa"
+$saCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $saUsername, $saPassword
+
+$octoUsername = "octopus"
+$octoPassword = $OctopusParameters["sqlOctopusPassword"] | ConvertTo-SecureString -AsPlainText -Force
+$octoCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $octoUsername, $octoPassword
+
+While (-not $allListening){
+    if (-not $SqlOnline){
+        $SqlOnline = Test-SQL -cred $saCred
+    }
+    if (-not $LoginsDeployed){
+        $LoginsDeployed = Test-SQL -cred $octoCred
+    }
+    if (-not $JumpboxListening){
+        $jumpboxes = Get-Tentacles -role $dbJumpboxRole
+        if ($jumpboxes.count = 1){
+            $JumpboxListening = $true
+        }
+    }
+    if ($IssInstalls -lt $numWebServers){
+        Write-Warning "To do: Figute out how to check IIS on all the web servers!"
+    }
+    if ($WebServersListening -lt $numWebServers){
+        $webServers = Get-Tentacles -role $webServerRole
+        if ($webServers.count -gt $WebServersListening){
+            Write-Warning "To do: Log which web server just came online!"
+            $WebServersListening = $webServers.count
+        }
     }
 
-    function Test-SQL {
-        param (
-            $ip,
-            $cred
-        )
-        try { 
-            Invoke-DbaQuery -SqlInstance $ip -Query 'SELECT @@version' -SqlCredential $cred -EnableException
-        }
-        catch {
-            return $false
-        }
-        return $true
+
+    $time = $time = [Math]::Floor([decimal]($stopwatch.Elapsed.TotalSeconds))
+    Write-Output "$time seconds | SQL- Installed: $SqlOnline, Logins: $LoginsDeployed, Jumpbox: $JumpboxListening | WEB- IIS: $IssInstalls, Tentacles: $WebServersListening"
+                 #Extracting package 'C:\Octopus\Tentacle\Files\RandomQuotes_SQL_infra@S0.0.53@7F26AF5DA0AB8B4FB2F4A449E5F141C3.nupkg' to 
+    if (($time -gt 60) -and (-not $runningWarningGiven)){
+        Write-Warning "EC2 instances are taking an unusually long time to start."
+        $runningWarningGiven = $true
     }
-
-    function Wait-ForConnection {
-        param (
-            $ipAddress,
-            $cred,
-            $waitMsg,
-            $successMsg,
-            $timeout
-        )
-        $connectionMade = $false
-        While (-not $connectionMade){   
-            $time = [Math]::Floor([decimal]($stopwatch.Elapsed.TotalSeconds))
-            if ($time -gt $timeout){
-                Write-Error "$time seconds: This is taking too long. Something is probably broken."
-            }    
-            try {
-                if (Test-SQL -ip $ipAddress -cred $saCred){
-                        $connectionMade = $true
-                        Write-Output "        $time seconds: $successMsg"
-                } 
-                else {
-                    Write-Output "      $time seconds: $waitMsg"
-                }
-            }
-            catch {
-                Write-Warning "something broke"
-            }
-            Start-Sleep -s 20
-        }
+    if ($time -gt $timeout){
+        Write-Error "Timed out at $time seconds. Timeout currently set to $timeout seconds. There is a parameter on this script to adjust the default timeout."
+    }    
+    
+    if (( $LoginsDeployed -and $JumpboxListening) -and ($WebServersListening -eq $WebServersListening)){
+        $allListening = $true
+        Write-Output "SUCCESS! Environment built successfully. SQL Server IP address is: $dbIpAddress"
+        break
     }
-
-    $saPassword = $OctopusParameters["sqlSaPassword"] | ConvertTo-SecureString -AsPlainText -Force
-    $saUsername = "sa"
-    $saCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $saUsername, $saPassword
-    $saWaitMsg = "SQL Server not online yet."
-    $saSuccessMsg = "SUCCESS: SQL Server is running and available at: $ipAddress"
-
-    Write-Output "    Waiting for SQL Server to come online. (Usually within 600 seconds.)"
-    Wait-ForConnection -ipAddress $ipAddress -cred $saCred -waitMsg $saWaitMsg -successMsg $saSuccessMsg -timeout 800
-
-    $octoUsername = "octopus"
-    $octoPassword = $OctopusParameters["sqlOctopusPassword"] | ConvertTo-SecureString -AsPlainText -Force
-    $octoCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $octoUsername, $octoPassword
-    $octoWaitMsg = "SQL logins not deployed yet."
-    $octoSuccessMsg = "SUCCESS: SQL Server logins deployed!"
-
-    Write-Output "    Waiting for SQL logins to be deployed. (Usually within 1200 seconds.)"
-    Wait-ForConnection -ipAddress $ipAddress -cred $octoCred -waitMsg $octoWaitMsg -successMsg $octoSuccessMsg -timeout 1400
+    Start-Sleep -s 15
 }
