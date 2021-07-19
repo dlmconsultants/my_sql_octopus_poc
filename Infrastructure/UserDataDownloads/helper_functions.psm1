9abcef425ddf820ca372242ff3f11b3586c248a4
Alex Yates <alex.yates@dlmconsultants.com>
2021-07-19 17:11:27 +0100

    Trying it the simple way

--- Infrastructure/UserDataDownloads/helper_functions.psm1
+++ Infrastructure/UserDataDownloads/helper_functions.psm1
@@ -5,11 +5,8 @@ Function Update-StatupStatus {
     $instanceMetaDataUri = "http://169.254.169.254/latest/meta-data/instance-id"
     $instanceId = Invoke-WebRequest -Uri $instanceMetaDataUri -TimeoutSec 1 -UseBasicParsing
   
-    Import-Module -name AWS.Tools.EC2
     # Add new StartupStatus tag
-    $tag = New-Object Amazon.EC2.Model.Tag
-    $tag.Key = "StartupStatus"
-    $tag.Value = $status
+    $tag = @( @{key="StartupStatus";value=$status} )
     New-EC2Tag -Resource $instanceId -Tag $tag # Actually replaces, if tag already exists
 }
 
