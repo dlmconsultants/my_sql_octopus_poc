1cece87d5018cec01c096e4c490c3dcf32c49a81
Alex Yates <alex.yates@dlmconsultants.com>
2021-07-19 16:54:57 +0100

    Importing module to load Amazon.EC2.Model.Tag assembly

--- Infrastructure/UserDataDownloads/helper_functions.psm1
+++ Infrastructure/UserDataDownloads/helper_functions.psm1
@@ -5,6 +5,7 @@ Function Update-StatupStatus {
     $instanceMetaDataUri = "http://169.254.169.254/latest/meta-data/instance-id"
     $instanceId = Invoke-WebRequest -Uri $instanceMetaDataUri -TimeoutSec 1 -UseBasicParsing
   
+    Import-Module -name AWS.Tools.EC2
     # Add new StartupStatus tag
     $tag = New-Object Amazon.EC2.Model.Tag
     $tag.Key = "StartupStatus"
