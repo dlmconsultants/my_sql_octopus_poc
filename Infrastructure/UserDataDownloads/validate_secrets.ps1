38ed4a61385750be3a126e4a412470d0994c5c06
Alex Yates <alex.yates@dlmconsultants.com>
2021-07-20 10:16:25 +0100

    logging passwords for troubleshooting. Delete later

--- Infrastructure/UserDataDownloads/validate_secrets.ps1
+++ Infrastructure/UserDataDownloads/validate_secrets.ps1
@@ -53,24 +53,28 @@ if ($missingSecrets.length -gt 0){
 # Checking some of the secrets are in the expected format
 if ("OCTOPUS_APIKEY" -notin $missingSecrets){
     if ($octopus_apikey -notlike "API-*"){
+        Write-Warning "OCTOPUS_APIKEY in AWS is: $octopus_apikey"
         $badSecretMessages = $badSecretMessages + "OCTOPUS_APIKEY does not start with ""API-"". "
     }
     if (-not ($octopus_apikey.length -eq 36)){
         $OctoApiKeyLength = $octopus_apikey.length
+        Write-Warning  "OCTOPUS_APIKEY in AWS is: $octopus_apikey"
         $badSecretMessages = $badSecretMessages + "OCTOPUS_APIKEY is not the correct length (Expected: 36 chars, Actual: $OctoApiKeyLength). "
     }
 }
 
 if (("OCTOPUS_SQL_PASSWORD" -notin $missingSecrets) -and ($octopus_sql_password -notlike $expectedOctopusSqlPassword)){
+    Write-Warning  "OCTOPUS_SQL_PASSWORD in AWS is: $octopus_sql_password"
+    Write-Warning  "OCTOPUS_SQL_PASSWORD in Octopus is: $expectedOctopusSqlPassword"
     $badSecretMessages = $badSecretMessages + "OCTOPUS_SQL_PASSWORD in Octopus and AWS do not match. "
 }
 
 # Logging password validation results
 if ($badSecretMessages -notlike ""){
-    $errorMessage = "$errorMesage. Also, there are problems with the following secrets: $badSecretMessages"
+    $errorMessage = "$errorMesage There are problems with the following secrets: $badSecretMessages"
 }
 if ($errorMessage -notlike ""){
-    Update-StatupStatus -status "FAILED-AwsSecretsValidationErrors-SeeLogIn-C/Startup"
+    Update-StatupStatus -status "FAILED-AwsSecretsValidationErrors-$errorMessage"
     Write-Error "$errorMessage"
 } else {
     Write-Output "All required AWS Secrets are present and pass validation checks."
