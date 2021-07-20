bc6643178b66360435e2743fc3b9e89aa2d07276
Alex Yates <alex.yates@dlmconsultants.com>
2021-07-20 14:35:52 +0100

    This function has been extracted to helper_functions

--- Infrastructure/UserDataDownloads/install_sql_server.ps1
+++ Infrastructure/UserDataDownloads/install_sql_server.ps1
@@ -1,3 +1,6 @@
+
+
+
 # updating the sa user
 Write-Host "Getting the name of the current user to replace in the copy ini file."
 $user = "$env:UserDomain\$env:USERNAME"
@@ -6,16 +9,6 @@ write-host $user
 Write-Host "Replacing the placeholder user name in Confifuration.ini with $user."
 $replaceText = (Get-Content -path "C:\Startup\scripts\ConfigurationFile.ini" -Raw) -replace "__MY_USER__", $user
 Set-Content "C:\Startup\scripts\ConfigurationFile.ini" $replaceText
-
-# updating the sa password
-function get-secret(){
-  param ($secret)
-  $secretValue = Get-SECSecretValue -SecretId $secret
-  # values are returned in format: {"key":"value"}
-  $splitValue = $secretValue.SecretString -Split '"'
-  $cleanedSecret = $splitValue[3]
-  return $cleanedSecret
-}
 
 Write-Host "Retrieving sa password from EC2 Secrets Manager and updating Confifuration.ini."
 $saPassword = Get-Secret -secret "SYSADMIN_SQL_PASSWORD"
