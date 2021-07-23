5aacc805f12bbeda6dbd8c536562b531f177e42a
Alex Yates <alex.yates@dlmconsultants.com>
2021-07-23 10:49:15 +0100

    moving the max 255 chars check to the Update-StartupStatus function

--- Infrastructure/UserDataDownloads/helper_functions.psm1
+++ Infrastructure/UserDataDownloads/helper_functions.psm1
@@ -4,6 +4,11 @@ Function Update-StatupStatus {
     )
     $instanceMetaDataUri = "http://169.254.169.254/latest/meta-data/instance-id"
     $instanceId = Invoke-WebRequest -Uri $instanceMetaDataUri -TimeoutSec 1 -UseBasicParsing
+    
+    # Tag values can be max 256 chars
+    if ($status.length -gt 255){
+        $status = $newStatus.SubString(0,213) + " ... (see log in c:/startupfor full error)" # additional string is 42 chars. 213 + 42 = 255
+    }
   
     # Add new StartupStatus tag
     $tag = @( @{key="StartupStatus";value=$status} )
