ccd38c795de09b50a2939ba122cbfb937a11c15d
Alex Yates <alex.yates@dlmconsultants.com>
2021-07-23 13:47:53 +0100

    Apparently the dotnet downloads are getting super flaky?

--- Infrastructure/UserDataDownloads/setup_dotnet_core.ps1
+++ Infrastructure/UserDataDownloads/setup_dotnet_core.ps1
@@ -15,8 +15,29 @@ $dotnetversion = "2.1.22"
 $dotnetUrl = "https://download.visualstudio.microsoft.com/download/pr/2fe0ef0c-a6b6-4cda-b6b8-874068bb131f/709d1c7817fa19524089dda74933ddce/dotnet-hosting-2.1.22-win.exe"
 $dotnetHostingBundleInstaller = "$startupDir/dotnet-hosting-$dotnetversion-win.exe"
 
+Write-Output "Downloading ASP.NET Core Runtime - Windows Hosting Bundle Installer v$dotnetversion"
+
 if ((test-path $dotnetHostingBundleInstaller) -ne $true) {
-    Download-File -url $dotnetUrl -SaveAs $dotnetHostingBundleInstaller 
+    try {
+      Download-File -url $dotnetUrl -SaveAs $dotnetHostingBundleInstaller 
+    }
+    catch {
+      # Apparently this download can sometimes be a bit flakey. Sometimes hitting a 404.
+      Write-Warning "First attempt to download dotnet core failed with error: $Error[0]"
+      Write-output "Trying 10 more times..."
+      for ($i = 1; $i -lt 11; $i++){
+        Start-Sleep 1
+        Write-output "Attempte $i / 10"
+        try {
+          Download-File -url $dotnetUrl -SaveAs $dotnetHostingBundleInstaller 
+          Write-Output "worked this time"
+          break
+        }
+        catch {
+          Write-Output "failed"
+        }  
+      }
+    }    
 }
 else {
     Write-Output "  dotnet core hosting bundle already downloaded to $dotnetHostingBundleInstaller"
