64f38e15fdf71cc3c947c25c8fb0c255bad52d1f
Alex Yates <alex.yates@dlmconsultants.com>
2021-07-27 13:48:25 +0100

    more defensive while loop

--- Infrastructure/helper_scripts/build_servers.ps1
+++ Infrastructure/helper_scripts/build_servers.ps1
@@ -377,7 +377,7 @@ $numRequiredInstances = $numTotalInstances - $numTerminatedInstances
 $numReadyInstances = ($instances | Where-Object {$_.status -like "ready*"}).id.count
 
 # Now we wait in a holding pattern until all instances have a status of either "ready*", or "terminated"
-while ($numRequiredInstances -ne $numReadyInstances){
+while ($numReadyInstances -lt $numRequiredInstances){
     Start-Sleep -s 2
     $time = [Math]::Floor([decimal]($stopwatch.Elapsed.TotalSeconds))
     foreach ($instanceId in $instances.id) {
